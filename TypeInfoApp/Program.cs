05ad7745a1092241fbbd55fa8cfd4b6ca5b04cee
lagbullet <40629577+lagbullet@users.noreply.github.com>
2018-08-09 09:40:03 +0300

    1

---
+++ TypeInfoApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeInfoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            TypeInfoClass Info = new TypeInfoClass("TypeInfoApp.TypeInfoClass");
+            Info.ShowAll();
+            Console.ReadKey();
+        }
+    }
+}
